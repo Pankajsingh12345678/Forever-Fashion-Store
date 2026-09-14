46b05b27ac7f3186f6f4f8f33adbecda7a2ad04c
Pankaj <pankajsingh4138@gmail.com>
2026-09-14 14:37:34 +0530

    Added footer section

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sit dolorum, quam reiciendis est id tempore ipsa veniam quas necessitatibus beatae qui enim porro magni cum nam eaque commodi neque?
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+             <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-212-456-7890</li>
+                <li>contact@foreverfashionshop.com</li>
+             </ul>
+
+        
+        </div>
+
+        <div className='col-span-3'>
+            <hr />
+            <p className='py-5 text-center'>Copyright 2026@ foreverfashionshop.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
