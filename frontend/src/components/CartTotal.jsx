e9cd88a55a0ef4873213d96444aebb21d98c0eb1
Pankaj <pankajsingh4138@gmail.com>
2026-09-20 14:59:51 +0530

    Add CartTotal component and update cart functionality

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Title from './Title';
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+
+
+const CartTotal = () => {
+
+    const {currency,delivery_fees,getCartAmount} = useContext(ShopContext);
+  return  (
+    <div className='w-full'>
+        <div className='text-2xl'>
+            <Title text1={'CART'} text2={'TOTALS'} />
+        </div>
+
+        <div classaName='flex flex-col gap-2 mt-2 text-sm'>
+
+            <div className='flex justify-between'>
+                <p>Subtotal</p>
+                <p>{currency}{getCartAmount()}.00</p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p>Shipping Fee</p>
+                <p>{currency} {delivery_fees}.00</p>
+            </div>
+
+            <hr />
+            <div className='fex justify-between'>
+                <b>Total</b>
+                <b>{currency} {getCartAmount () === 0 ? 0 : getCartAmount() + delivery_fees}</b>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default CartTotal
